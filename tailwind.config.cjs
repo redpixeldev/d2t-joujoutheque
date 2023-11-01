712db20684543be041ebee09e0deb487bf825c11
Vivian Guillen <me@vivian.do>
2023-11-01 11:28:06 -0400

    Add forms

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -100,5 +100,5 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/typography')],
+	plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 };
